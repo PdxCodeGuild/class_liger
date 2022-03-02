c75dc50c0e442da24c028153a220cf21858f107a
Michael Marshall <mrmarsh2@gmail.com>
2022-03-01 21:26:48 -0700

    lab and practice for the day

---
+++ code/michael/python/01practice.py
@@ -0,0 +1,18 @@
+# animal = 'dog'
+# print(animal)
+# print(type(animal))
+# print(animal + '-dick')
+# animal += 'fish'
+# print(animal)
+# # animal = animal.capitalize()
+
+# print(animal.upper())
+# print(animal.lower())
+
+# print(animal.replace('o', '0'))
+# print(animal.replace('i', '1').upper().split())
+
+a = 5
+b = 9
+
+print(a == 5 ^ b == 5)
