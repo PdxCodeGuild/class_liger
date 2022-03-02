773630120034ae4b11cd9f1dd8e1711317cfaf27
santorsm <matt.santorsola@gmail.com>
2022-03-01 20:13:51 -0800

    posting lab 02

---
+++ code/matt/lab02.py
@@ -0,0 +1,18 @@
+# Lab 02 Mad Libs
+
+
+noun1 = input("Please enter a noun: ")
+noun2 = input("Please enter a noun: ")
+noun3 = input("Please enter a noun: ")
+noun4 = input("Please enter a noun: ")
+noun5 = input("Please enter a noun: ")
+
+
+print(
+    f"""
+            A {noun1.upper()} is a staple of any summer wardrobe.
+            Be careful not to vacuum the {noun2.upper()} when cleaning your room.
+            BBQ at my house! Everyone's invited, and don't forget it's bring your own {noun3.upper()}!
+            I like my {noun4.upper()} with mustard, relish, and {noun5.upper()}.
+        """
+)
