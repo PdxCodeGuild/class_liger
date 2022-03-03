9978339882a9adca01ec9ef9d26debe18c3e2322
clement2022 <clement.buadu@gmail.com>
2022-03-02 22:08:14 -0500

    adding lab2.py

--- code/clement/lab2.py
+++ code/clement/lab2.py
@@ -6,8 +6,9 @@ verb_1 = input("Please enter a verb (present tense):\n")
 verb_2 = input("Enter another present tense:\n")
 
 
-print("Welcome to Mad Lib Game!")
-print("Today, every programmer at least has a small enough laptop to fit in their " + noun)
-print("They can solve any math problem with their programming " + noun_choice)
-print("Programmer can add, multiply, substract, and " + verb_1)
-print("They can " + verb_2 "smater than an average human.")
+print(f"Welcome to Mad Lib Game!")
+print(
+    f"Today, every programmer at least has a small enough laptop to fit in their {noun}")
+print(f"They can solve any math problem with their programming {noun_choice}")
+print(f"Programmer can add, multiply, substract, and  {verb_1}")
+print(f"They can {verb_2} smater than an average human")
