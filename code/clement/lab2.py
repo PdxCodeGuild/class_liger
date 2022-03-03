43b71893b3a40f11fa8893bdfa3f476eb486d7f9
clement2022 <clement.buadu@gmail.com>
2022-03-02 20:51:21 -0500

    adding lab2.py

--- code/clement/lab2.py
+++ code/clement/lab2.py
@@ -1,3 +1,13 @@
 
 
-print("Thanks again Mr. Keegan")
+noun = input("Enter a noun!\n")
+noun_choice = input("Enter a plural noun:\n")
+verb_1 = input("Please enter a verb (present tense):\n")
+verb_2 = input("Enter another present tense:\n")
+
+
+print("Welcome to Mad Lib Game!")
+print("Today, every programmer at least has a small enough laptop to fit in their " + noun)
+print("They can solve any math problem with their programming " + noun_choice)
+print("Programmer can add, multiply, substract, and " + verb_1)
+print("They can " + verb_2 "smater than an average human.")
