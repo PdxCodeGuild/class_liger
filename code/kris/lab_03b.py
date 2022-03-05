96c3f22d631048d394241426274a3f344cb86d0f
Kris Hernandez <krismac@Kriss-MacBook-Air.local>
2022-03-05 00:46:33 -0500

    saved...again

--- code/kris/lab_03b.py
+++ code/kris/lab_03b.py
@@ -1,28 +1,26 @@
 
 numbers = [5, 1, 8, 3, 4, 1, 6]
 added_number = 0
+averaged_number = []
 for number in numbers:
     added_number += number
+    averaged_number.append(added_number)
+    
+    # print(added_number)
+    print(averaged_number)
+
+
+for i in range(len(numbers)):
     print(added_number)
     
 
 
-for i in range(len(numbers)):
+# # while True: 
+# #     numbers_wanted = input("\nEnter a number or type 'done' to quit: ")
     
-
-
-
-
-
-
-
-
-# while True: 
-#     numbers_wanted = input("\nEnter a number or type 'done' to quit: ")
-    
-#     if numbers_wanted == 'done':
-#         averaged_numbers =  sum(numbers)/len(numbers)
-#         print(f"\nThe average is {averaged_numbers}")
-#         break
-#     else: 
-#         numbers.append(int(numbers_wanted))
+# #     if numbers_wanted == 'done':
+# #         averaged_numbers =  sum(numbers)/len(numbers)
+# #         print(f"\nThe average is {averaged_numbers}")
+# #         break
+# #     else:
+# #         numbers.append(int(numbers_wanted))
