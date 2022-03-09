a1ff0ad51bcdd13e373a99e371a47dbd445140e8
santorsm <matt.santorsola@gmail.com>
2022-03-08 21:38:31 -0800

    lab 05 - initial commit

---
+++ code/matt/python/lab05.py
@@ -0,0 +1,24 @@
+# Lab 05 Palindrome Checker
+
+"""
+A palindrome is a word that's the same forwards or backwards, e.g. racecar. Another way to think of it is as a word that's equal to its own reverse.
+
+Write a function check_palindrome which takes a string, and returns True if the string's a palindrome, or False if it's not.
+"""
+
+# user enter word - ? - spaces
+# create palindrome function
+# user input function argument
+# reverse the word
+# check if the word =s its reverse
+# - if equal says its a palindrome
+# - if not equal say its not a palindrome
+
+x = "test"
+
+
+def check_palindrome(word: str) -> str:
+    print(x)
+
+
+check_palindrome(x)
