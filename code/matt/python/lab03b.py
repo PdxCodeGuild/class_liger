1d5d8623488207c2dd6143eae9ff0975b5c74ad2
santorsm <matt.santorsola@gmail.com>
2022-03-08 21:13:50 -0800

    lab 03b - version 1

--- code/matt/python/lab03b.py
+++ code/matt/python/lab03b.py
@@ -1,8 +1,21 @@
 # Lab 03b Average Number
 
 
-def avg_num_v1():
+# function to calculate average
+nums1 = [5, 0, 8, 3, 4, 1, 6]
+
+
+def avg_num_v1(nums: list) -> float:
     pass
+    running_total = 0
+    for num in nums:
+        running_total += num
+        the_average = running_total / len(nums)
+        the_average = round(the_average, 2)
+    return the_average
+
+
+print(avg_num_v1(nums1))
 
 
 def avg_num_v2():
