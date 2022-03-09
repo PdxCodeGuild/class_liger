08d6b0073b7804ebf95c0a8b876267904e99ee5e
santorsm <matt.santorsola@gmail.com>
2022-03-08 18:42:33 -0800

    lab 02b

--- code/matt/python/lab02b.py
+++ code/matt/python/lab02b.py
@@ -29,20 +29,27 @@ coins = {"quarter": 25, "dime": 10, "nickel": 5, "penny": 1}
 # empty dictionary to hold coin names and number
 change_dict = {}
 
+# loop through coin names to obtain key values
 for key in coins.keys():
+
+    # running total of value to subtract from dollar amount
     amount_removed = 0
 
+    # allows loop to continue traversing dictionary when a coin prior to the end is not used
     if amount // coins[key] == 0:
         continue
 
+    # builds # of coins & subtracts amount removed from dollar amount
     elif amount > 0 and amount // coins[key] > 0:
         coin_number = amount // coins[key]
         amount_removed = coin_number * coins[key]
         amount -= amount_removed
-
+    # populates empty dictionary with coin name (key) and number of coins.
+    # Only allows keys that do not already exist in the dictionary
     if key not in change_dict:
         change_dict.update({key: coin_number})
 
+# results output
 print("Your change is: ")
 for key in change_dict:
     print(f"\t{int(change_dict[key])} {key}")
