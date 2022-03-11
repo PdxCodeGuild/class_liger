e32c616284fcb4e6ec10fdbb71db99570030a4a9
Kris Hernandez <krismac@Kriss-MacBook-Air.local>
2022-03-11 00:17:05 -0500

    lab 02b revised

--- code/kris/lab_02b_make_change.py
+++ code/kris/lab_02b_make_change.py
@@ -1,6 +1,4 @@
 
-original_amount = float(input("\nHow much do you want to convert?(ex. 1.36): "))
-amount_in_pennies = original_amount * 100
 def change(amount_in_pennies): 
 
     quarters = amount_in_pennies // 25
@@ -11,39 +9,24 @@ def change(amount_in_pennies):
     nickels_remained = dimes_remained % 5
     pennies = nickels_remained / 1
 
+    output = ''
     if quarters >= 1: 
-        quarters_total = (str(quarters) + " quarters")
-    else: 
-        quarters_total = "0 quarters"
+        output = output + f"{quarters} quarters "
     if dimes >= 1:
-        dimes_total = (str(dimes) + " dimes")
-    else:
-        dimes_total = "0 dimes"
+        output = output + f"{dimes} dimes "
     if nickels >= 1:
-        nickels_total = (str(nickels) + " nickels")
-    else:
-        nickels_total = "0 nickels"
+        output = output + f"{nickels} nickels "
     pennies = round(pennies)
     if pennies >= 1:
-        pennies_total = (str(pennies) + " pennies")
-    else:
-        pennies_total = "0 pennies"
+        output = output + f"{pennies} pennies"
     
-    total_change_list = [quarters_total, dimes_total, nickels_total, pennies_total]
-
-    if quarters == 0:
-        total_change_list.remove(quarters_total)
-    if dimes == 0:
-        total_change_list.remove(dimes_total)
-    if nickels == 0:
-        total_change_list.remove(nickels_total)
-    if pennies == 0:
-        total_change_list.remove(pennies_total)
+    return output
     
+original_amount = float(input("\nHow much do you want to convert?(ex. 1.36): "))
 
+amount_in_pennies = original_amount * 100
 
-    return  total_change_list
-
+amount_in_pennies = int(amount_in_pennies)
 
 change_recieved = change(amount_in_pennies)
-print(change_recieved)
+print(f"\nYour change is {change_recieved}")
