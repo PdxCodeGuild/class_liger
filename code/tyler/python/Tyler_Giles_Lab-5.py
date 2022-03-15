0d7b6dcd347965f74d608f8d15a33696cdd7ad41
CardinalKibbles <tylerjaygiles@gmail.com>
2022-03-14 18:25:04 -0700

    Update Tyler_Giles_Lab-5.py

--- code/tyler/python/Tyler_Giles_Lab-5.py
+++ code/tyler/python/Tyler_Giles_Lab-5.py
@@ -3,8 +3,6 @@
 
 
 user_attempt = input("Enter a word: ")
-
-
 user_attempt = list(user_attempt)
 reversed_input = list(user_attempt)
 reversed_input == reversed_input.reverse()
@@ -12,8 +10,6 @@ reversed_input == reversed_input.reverse()
 if user_attempt == reversed_input:
     print(f"\nWow. {user_attempt} is a palindrome.")
     
-
 else:
     print(f"\n{user_attempt} is not a palindrome. ")
-
 
