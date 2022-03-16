1746918612f48354be08ba747b7fb11c156ba5fd
CardinalKibbles <tylerjaygiles@gmail.com>
2022-03-15 21:20:18 -0700

    Update Tyler_Giles_Lab-6b.py

--- code/tyler/python/Tyler_Giles_Lab-6b.py
+++ code/tyler/python/Tyler_Giles_Lab-6b.py
@@ -1,5 +1,4 @@
-
-user_credit_input = (input("Please enter your 16 digit credit card number to check validity: "))
+user_credit_input = (input("Please enter your 16 digit credit card number to check validity: "))         # Exaxmple number 4556737586899855
 # convert the user CC to a list of ints
 user_credit_input = list(map(int, user_credit_input))
 
@@ -17,16 +16,19 @@ elif len(user_credit_input) == 16:
 
     # slice off last digit to become the "check digit"
     check_digit = user_credit_input.pop(-1)
-
-
-
-    user_credit_input.reverse()
-
     print(check_digit)
 
-
+    user_credit_input.reverse()
     print(user_credit_input)
 
-    
+    user_credit_input[::2] = [i * 2 for i in user_credit_input[::2]]
+    print(user_credit_input)
+
+    user_credit_input = [i - 9 if i > 9 else i for i in user_credit_input]
+    print(user_credit_input)
+
+    print(sum(user_credit_input))
+    if sum(user_credit_input) % 10 == check_digit:
+        print("Valid")
 
 
