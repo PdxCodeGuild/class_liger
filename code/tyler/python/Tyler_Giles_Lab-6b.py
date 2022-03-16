67404bebe082c9ed97811c33c1ca00979c96bedf
CardinalKibbles <tylerjaygiles@gmail.com>
2022-03-15 20:15:09 -0700

    Update Tyler_Giles_Lab-6b.py

--- code/tyler/python/Tyler_Giles_Lab-6b.py
+++ code/tyler/python/Tyler_Giles_Lab-6b.py
@@ -1,11 +1,32 @@
 
-user_credit_input = (input("Please enter your credit card number to check validity: "))
+user_credit_input = (input("Please enter your 16 digit credit card number to check validity: "))
 # convert the user CC to a list of ints
 user_credit_input = list(map(int, user_credit_input))
-# slice off last digit to become the "check digit"
-check_digit = user_credit_input.pop(-1)
 
-# print(check_digit)
+# catch inputs of less than or greater than 16 to keep programming running correctly
+if len(user_credit_input) < 16:
+    print("Please try again and enter a 16 digit CC number.")
+
+elif len(user_credit_input) > 16:
+    print("Please try again and enter a 16 digit CC number.")
 
 
-print(user_credit_input)
+# code block for correct amount of digits
+
+elif len(user_credit_input) == 16:
+
+    # slice off last digit to become the "check digit"
+    check_digit = user_credit_input.pop(-1)
+
+
+
+    user_credit_input.reverse()
+
+    print(check_digit)
+
+
+    print(user_credit_input)
+
+    
+
+
