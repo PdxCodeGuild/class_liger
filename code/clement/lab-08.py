fa3ce40db9d3984a555e5bd65768c334dbc9f200
Clement2022 <clement.buadu@gmail.com>
2022-03-18 00:38:03 -0400

    updating lab-08

--- code/clement/lab-08.py
+++ code/clement/lab-08.py
@@ -8,11 +8,14 @@ def pick_six():
         pick_nums = random.randint(1, 100)
         ticket.append(pick_nums)
     return ticket
-
+    
+def investment_return(ticket_winning, expenses):
+    return ((ticket_winning - expenses) / expenses)
 
 winn_numbers = pick_six()
 ticket_winning = 0
-ticket_bal = 0
+cost_ticket = 2
+num_of_tickets = 100000
 
 """
 loop through the indices of the list, print each
@@ -20,7 +23,7 @@ use each index to print the value at that index (from both lists)
 compare the values at each index and count how many match
 """
 
-for game in range(100000):
+for game in range(num_of_tickets):
     matches_num = pick_six()
     lott_count = 0
     for i in range(6):
@@ -37,21 +40,20 @@ for game in range(100000):
     elif lott_count == 4:
         ticket_winning = ticket_winning + 50000
     elif lott_count == 5:
-        ticket_winning = ticket_winning + 100000
+        ticket_winning = ticket_winning + 1000000
     elif lott_count == 6:
-        ticket_winning = ticket_winning + 250000
+        ticket_winning = ticket_winning + 25000000
     # print(f"You have {lott_count} matches")
    
 
-    ticket_winning = ticket_winning  - 2
+    # ticket_winning = ticket_winning  - 2
 
 # option 2 on pick6 lab............................
 
-def investment_return(n1, n2):
-    return (n1 - n2)
 
-expenses = 2
-earnings = (ticket_winning - expenses) // expenses
+
+expenses = cost_ticket * num_of_tickets
+earnings = investment_return(ticket_winning, expenses)
 
 
 
