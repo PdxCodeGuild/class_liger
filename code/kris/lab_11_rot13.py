13ad7e9be113937742c96d6e5a822603d95b5ab2
Kris Hernandez <krismac@Kriss-MacBook-Air.local>
2022-03-19 00:35:48 -0400

    kris lab 11 working

---
+++ code/kris/lab_11_rot13.py
@@ -0,0 +1,17 @@
+
+from string import ascii_lowercase as alphabet
+alphabet = list(alphabet)
+coded_word = ''
+# needs_code = list(input("Word or phrase to code: "))``
+a_index = 0
+print(alphabet[a_index + 13])
+    
+# for indexed_letters in range(len(alphabet)):
+#     print(indexed_letters)
+# for charcater in needs_code:
+#     if charcater in alphabet:
+#         coded_word = coded_word + alphabet[charcater + 13]
+#     else:
+#         coded_word = coded_word + charcater
+
+# print(coded_word)
