04841597b03303940efdd3ab0462343207d6516e
CardinalKibbles <tylerjaygiles@gmail.com>
2022-03-22 20:47:22 -0700

    Update Tyler_Giles_Lab-8.py

--- code/tyler/python/Tyler_Giles_Lab-8.py
+++ code/tyler/python/Tyler_Giles_Lab-8.py
@@ -1,7 +1,17 @@
 import random
 
-winning_ticket = []
+def pick6():
+    balance = 0
+    winning_ticket = []  
+    tickets_bought = 0
+    # print("\nGenerating winning numbers and tickets... ")
+    while tickets_bought < 100000:                              
+        if len(winning_ticket) < 6:
+            winning_ticket.append(random.randint(1, 99))
+        tickets_bought += 1
+        balance -= 2
+        
+    print(winning_ticket)
 
-while len(winning_ticket) < 6:
-    winning_ticket.append(random.randint(1, 99))
 
+pick6()
