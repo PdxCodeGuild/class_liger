1d6d3adf66aff33f336a80ec7caa6ef5b5168671
CardinalKibbles <tylerjaygiles@gmail.com>
2022-03-22 21:20:00 -0700

    Update Tyler_Giles_Lab-8.py

--- code/tyler/python/Tyler_Giles_Lab-8.py
+++ code/tyler/python/Tyler_Giles_Lab-8.py
@@ -1,17 +1,22 @@
 import random
 
 def pick6():
+    
     balance = 0
     winning_ticket = []  
     tickets_bought = 0
-    # print("\nGenerating winning numbers and tickets... ")
+    
+    print("\nGenerating winning numbers and tickets... ")
+    
     while tickets_bought < 100000:                              
         if len(winning_ticket) < 6:
             winning_ticket.append(random.randint(1, 99))
         tickets_bought += 1
         balance -= 2
-        
-    print(winning_ticket)
+
+
+    
+    print(f"The winning numbers are: {winning_ticket[0]}, {winning_ticket[1]}, {winning_ticket[2]}, {winning_ticket[3]}, {winning_ticket[4]}, and {winning_ticket[5]}! ")
 
 
 pick6()
