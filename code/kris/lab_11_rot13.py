97721e56e5165d5729db5a4323e5123a86413ee5
Kris Hernandez <krismac@Kriss-MacBook-Air.local>
2022-03-22 23:49:03 -0400

    kris_python_lab-11

--- code/kris/lab_11_rot13.py
+++ code/kris/lab_11_rot13.py
@@ -2,16 +2,18 @@
 from string import ascii_lowercase as alphabet
 alphabet = list(alphabet)
 coded_word = ''
-# needs_code = list(input("Word or phrase to code: "))``
-a_index = 0
-print(alphabet[a_index + 13])
-    
-for index in range(len(alphabet)):
-    print(index)
-# for charcater in needs_code:
-#     if charcater in alphabet:
-#         coded_word = coded_word + alphabet[charcater + 13]
-#     else:
-#         coded_word = coded_word + charcater
+test_word = input("\nWhat would you like to code?: ")
+times_rotated = int(input("\nHow many rotations?(ex. 13): "))
+test_word = test_word.lower()  
 
-# print(coded_word)
+
+for charcater in test_word:
+    if charcater in alphabet:
+        coded_letter = (alphabet.index(charcater) + times_rotated) % 26
+        coded_word = coded_word + alphabet[coded_letter]
+
+    else:
+        coded_word = coded_word + charcater
+
+print(f'''\nCoded!: 
+{coded_word}''')
