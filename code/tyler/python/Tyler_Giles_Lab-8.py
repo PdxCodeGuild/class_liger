ecde3909401e2324900b4dcad23e5861baffa155
CardinalKibbles <tylerjaygiles@gmail.com>
2022-03-24 21:31:42 -0700

    Update Tyler_Giles_Lab-8.py

--- code/tyler/python/Tyler_Giles_Lab-8.py
+++ code/tyler/python/Tyler_Giles_Lab-8.py
@@ -1,22 +1,37 @@
+# buy 100k tickets then pick6 to win
+# check each purchase everytime and compare winnings before buying a new ticket in the 100k loop
 import random
-
-def pick6():
-    
-    balance = 0
-    winning_ticket = []  
-    tickets_bought = 0
-    
-    print("\nGenerating winning numbers and tickets... ")
-    
-    while tickets_bought < 100000:                              
-        if len(winning_ticket) < 6:
-            winning_ticket.append(random.randint(1, 99))
-        tickets_bought += 1
-        balance -= 2
-
-
-    
-    print(f"The winning numbers are: {winning_ticket[0]}, {winning_ticket[1]}, {winning_ticket[2]}, {winning_ticket[3]}, {winning_ticket[4]}, and {winning_ticket[5]}! ")
+total_balance = 0
+losing_balance = 0
+winning_balance = 0
+winning_ticket = [] 
+playing_ticket = []
+tickets_bought = 0
+ticket_matches = 0
+match_payout_prices = {1 : 4,
+2 : 7,
+3 : 100,
+4 : 50000,
+5 : 1000000,
+6 : 25000000}
 
 
-pick6()
+# while len(winning_ticket) < 6:
+#     winning_ticket.append(random.randint(1, 99))
+#     playing_ticket.append(random.randint(1, 99))
+
+
+
+
+for i in range(len(winning_ticket)):
+    if playing_ticket[i] == winning_ticket[i]:
+        ticket_matches += 1
+
+
+        
+
+print(ticket_matches)
+# print(winning_balance)
+# print(playing_ticket)
+# print(winning_ticket)
+# print(f"${winning_balance}")
