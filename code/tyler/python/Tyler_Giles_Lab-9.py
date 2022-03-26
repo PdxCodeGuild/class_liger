996137ddbbd3b81203152449474507799ec2a43f
CardinalKibbles <tylerjaygiles@gmail.com>
2022-03-26 13:44:19 -0700

    Update Tyler_Giles_Lab-9.py

--- code/tyler/python/Tyler_Giles_Lab-9.py
+++ code/tyler/python/Tyler_Giles_Lab-9.py
@@ -1,6 +1,7 @@
 # Let's write a python program to give basic blackjack playing advice during a game by asking the player for cards.
 # First, ask the user for three playing cards (A, 2, 3, 4, 5, 6, 7, 8, 9, 10, J, Q, or K).
-# Then, figure out the point value of each card individually. Number cards are worth their number, all face cards are worth 10. At this point, assume aces are worth 1.
+# Then, figure out the point value of each card individually. 
+# Number cards are worth their number, all face cards are worth 10. At this point, assume aces are worth 1.
 
 card_values = {                                                             # dict of card values to add
 'A' : 1,
@@ -30,11 +31,17 @@ user_hand.append(user_card2.upper())
 user_card3 = input("What is your third card? ")
 user_hand.append(user_card3.upper())
 
-
-print(user_hand)
-
 for i in range(len(user_hand)):                                         # loop through the user hand list and add them up using the dict key value pairs
     points += card_values[user_hand[i]]                                 # using the user hand list as the key
 
-if points < 17:
-    print(f"You have {str(points)} points. Best move is Hit. ")
+if points < 17:                                                         # if statements for checking the score of your cards
+    print(f"You have {str(points)} points. Best move is Hit. ")          # I would like to change this into a while loop or for loop once I am further along
+
+elif points > 21:
+    print(f"You have {str(points)} points. Sorry you busted. ")
+
+elif points >= 17 and points < 21:
+    print(f"You have {str(points)} points. Best move is Stay. ")
+
+elif points == 21:
+    print(f"You got a Blackjack! Congrats you win! ")
