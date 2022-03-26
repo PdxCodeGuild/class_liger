2fdb5b94ecf794cfe9174bf720cc04d3bc0de581
CardinalKibbles <tylerjaygiles@gmail.com>
2022-03-25 21:30:31 -0700

    Update Tyler_Giles_Lab-8.py

--- code/tyler/python/Tyler_Giles_Lab-8.py
+++ code/tyler/python/Tyler_Giles_Lab-8.py
@@ -1,54 +1,58 @@
-# buy 100k tickets then pick6 to win
-# check each purchase everytime and compare winnings before buying a new ticket in the 100k loop
 import random
-total_balance = 0
-losing_balance = 0
+balance = 0
 winning_balance = 0
 winning_ticket = [] 
 playing_ticket = []
 tickets_bought = 0
-ticket_matches = 0
+
 match_payout_prices = { 
-1 : 4,
-2 : 7,
-3 : 100,
-4 : 50000,
-5 : 1000000,
-6 : 25000000}
-
-# Generates Winning ticket
-while len(winning_ticket) < 6:
-    winning_ticket.append(random.randint(1, 99))
+'1' : 4,
+'2' : 7,
+'3' : 100,
+'4' : 50000,
+'5' : 1000000,
+'6' : 25000000}
 
 
-# buys a ticket 10x
-while tickets_bought < 10:
+def pick6():
+    ticket = []
+    while len(ticket) < 6:
+        ticket.append(random.randint(1, 99))
+    
+    return ticket
+
+winning_ticket = pick6()
+
+
+while tickets_bought < 100000:
+    pick6()
+    playing_ticket = pick6()
     tickets_bought += 1
-    # print(tickets_bought)
-# generates random playing ticket
-    while len(playing_ticket) < 6:
-        playing_ticket.append(random.randint(1, 99))
-    # print(playing_ticket)
+    balance += 2
+    ticket_matches = 0
+    for i in range(len(playing_ticket)):
+        if playing_ticket[i] == winning_ticket[i]:
+            ticket_matches += 1
 
 
-for i in range(len(winning_ticket)):
-    if playing_ticket[i] == winning_ticket[i]:
-        ticket_matches += 1
+    if ticket_matches == 1:
+        winning_balance += match_payout_prices['1']
+
+    elif ticket_matches == 2:
+        winning_balance += match_payout_prices['2']
+
+    elif ticket_matches == 3:
+        winning_balance += match_payout_prices['3']
+
+    elif ticket_matches == 4:
+        winning_balance += match_payout_prices['4']
+
+    elif ticket_matches == 5:
+        winning_balance += match_payout_prices['5']
+
+    elif ticket_matches == 6:
+        winning_balance += match_payout_prices['6']
+            
+print(f"You spent ${balance}, and won ${winning_balance}. You have a staggering ROI of {(winning_balance - balance) / balance}! ")
         
 
-# comparing matches and increasing match variable
-
-
-print(winning_ticket)
-print()
-# print(playing_ticket)
-print(ticket_matches)
-
-
-
-        
-# print(tickets_bought)
-# print(winning_balance)
-# print(playing_ticket)
-# print(winning_ticket)
-# print(f"${winning_balance}")
