afc9c0909d56c885902a1c7b4dc6bdaf01210aec
CardinalKibbles <tylerjaygiles@gmail.com>
2022-03-28 18:35:51 -0700

    Create Lab 10

---
+++ code/tyler/python/Tyler_Giles_Lab-10.py
@@ -0,0 +1,5 @@
+# Use the Dad Joke API to get a dad joke and display it to the user. You may want to also use time.sleep to add suspense.
+# Use the requests library to send an HTTP request to https://icanhazdadjoke.com/ with the accept header as application/json. 
+# This will return a dad joke in JSON format. You can then use the .json() method on the response to get a dictionary. 
+# Get the joke out of the dictionary and show it to the user.
+
