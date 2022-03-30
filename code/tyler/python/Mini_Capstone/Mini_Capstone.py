216ddfee37c3a41c88cd0abf72c58e9136fb4eac
CardinalKibbles <tylerjaygiles@gmail.com>
2022-03-29 21:02:08 -0700

    rename
    fixing file names for clarity and loop readability

--- code/tyler/python/Mini_Capstone/Mini_Capstone.py
+++ code/tyler/python/Mini_Capstone/Mini_Capstone.py
@@ -9,10 +9,26 @@
 from colorama import init
 init()
 from colorama import Fore, Back, Style
+import os
 
-print(Back.GREEN, Fore.BLACK + 'File is pretty small ')
+current_directory = os.getcwd()
+
+folder_path = "/code/tyler/python/Mini_Capstone/data"
+folder_path = os.getcwd() + folder_path
+
+for file_name in os.listdir(folder_path):
+    if file_name.endswith('.txt'):
+        file_path = os.path.join(folder_path, file_name)
+        with open(file_path, 'r') as f:
+            print(Back.GREEN, Fore.BLACK +'ez clap ' + Style.RESET_ALL)
+
+
+print(Back.GREEN, Fore.BLACK + 'File is very small ')
 print(Style.RESET_ALL)
 print(Back.YELLOW, Fore.BLACK + 'File is pretty big ')
 print(Style.RESET_ALL)
 print(Back.RED, Fore.BLACK + 'File is very big ')
 print(Style.RESET_ALL)
+
+size = os.path.getsize('C:/Users/user/Documents/PDX_Code/class_liger/code/tyler/python/Mini_Capstone/data') 
+print('Size of file is', size, 'bytes')
