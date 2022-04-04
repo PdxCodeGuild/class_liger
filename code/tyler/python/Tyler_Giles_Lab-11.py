7d89eb62278ecb6ac4828f64f85b3f8417ad3310
Tyler Giles <tylerjaygiles@gmail.com>
2022-04-04 01:40:24 -0700

    Create Tyler_Giles_Lab-11.py

---
+++ code/tyler/python/Tyler_Giles_Lab-11.py
@@ -0,0 +1,7 @@
+# Version 1
+# Write a program that prompts the user for a string, and encodes it with ROT13. For each character, 
+# find the corresponding character, add it to an output string. 
+# Notice that there are 26 letters in the English language, so encryption is the same as decryption.
+
+from string import ascii_lowercase
+alphabet = ascii_lowercase
