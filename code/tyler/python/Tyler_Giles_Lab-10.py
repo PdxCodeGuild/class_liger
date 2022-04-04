c04fa49ea93d5e4d2f956977ad6f240c0a4bf7f2
Tyler Giles <tylerjaygiles@gmail.com>
2022-04-04 00:52:39 -0700

    Finished Lab 10

--- code/tyler/python/Tyler_Giles_Lab-10.py
+++ code/tyler/python/Tyler_Giles_Lab-10.py
@@ -8,3 +8,8 @@
 
 import requests
 import json
+url = 'https://icanhazdadjoke.com'
+headers = {'Accept' : 'application/json'}
+response = requests.get(url, headers = headers)
+data = response.json()
+print(data['joke'])
