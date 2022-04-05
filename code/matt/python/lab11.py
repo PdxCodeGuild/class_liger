e8fc40818e4f5ac792ba39d6be7fe40cf892ff4b
santorsm <matt.santorsola@gmail.com>
2022-04-04 20:57:07 -0700

    lab 11 resubmit 1

--- code/matt/python/lab11.py
+++ code/matt/python/lab11.py
@@ -2,6 +2,7 @@
 Lab 11
 
 version 1 & 2
+rework
 """
 
 import string
@@ -9,6 +10,7 @@ import string
 
 items = " "
 location = 0
+values_for_key_lookup = []
 
 
 def lower_alphabet():
@@ -16,66 +18,44 @@ def lower_alphabet():
     return list(string.ascii_lowercase)
 
 
-def get_value(letter: str, rotation: int = 13) -> int:
-    """Returns value associated with letter key value from letter_dict. Corrects for values greater than 25"""
-    if rotation < (max_value / 2) - 1:
-        letter_value = letter_dict.get(letter)
-        # print(rotation)
-        # print(f"{letter}, {letter_value}")
-        if letter_value > rotation:
-            letter_value = letter_value - rotation
-            return letter_value
-        else:
-            return letter_value + rotation
-    else:
-        letter_value = letter_dict.get(letter)
+letters = lower_alphabet()
 
-        if letter_value + rotation < max_value:
-            letter_value = letter_value + rotation
-            # print("get value: ", letter_value)
-            return letter_value
-        else:
-            # print(f"{letter}, {letter_value}")
-            return letter_value + rotation - max_value
+letter_dict = {letters[i]: i + 1 for i in range(0, len(letters), 1)}
+
+rot_letter_dict = letter_dict.copy()
 
 
-def get_key(val: int) -> str:
-    """Returns letter (key) based on the adjustments applied in get_value"""
-    for key, value in letter_dict.items():
-        if val == value:
-            return key
-    return f"{val} no key"
+word = input("Enter the word to encode: ")
+rotn = int(input("Enter the rotation used: "))
 
+original_input = word
 
-def ciphered_word(user_input: list[str]) -> str:
-    """Prints output ciphered by rotation input by user"""
+for item in word:
+    if item in items:
+        location = word.find(items)
+        word = word.replace(item, "")
+        # print(word)
+
+word_as_list = list(word)
+
+for key, value in letter_dict.items():
+    rot_letter_dict[key] = letter_dict[key] + rotn
+
+    if rot_letter_dict[key] > 26:
+        rot_letter_dict[key] = rot_letter_dict[key] - 26
+# print(rot_letter_dict)
+
+for letter in word_as_list:
+    # print(f"rotated valued: {rot_letter_dict.get(letter)}")
+    values_for_key_lookup.append(rot_letter_dict.get(letter))
     cipher = []
-    for letter in word_as_list:
-        value = get_value(letter, rotn)
-        cipher.append(get_key(value))
-    if location > 0:
-        cipher.insert(location, " ")
-    cipher = " ".join([str(item) for item in cipher])
-    print(f"\n'{original_input}' ciphered using ROT{rotn}: {cipher}")
+    for number in values_for_key_lookup:
+        for key, value in letter_dict.items():
+            if number == value:
+                # print(f"key: {key}")
+                cipher.append(key)
 
-
-if __name__ == "__main__":
-
-    letter_dict = {lower_alphabet()[i]: i for i in range(0, len(lower_alphabet()), 1)}
-    max_value = len(lower_alphabet())
-
-    word = input("Enter the word to encode: ")
-    rotn = int(input("Enter the rotation used: "))
-    original_input = word
-
-    for item in word:
-
-        if item in items:
-
-            location = word.find(items)
-
-            word = word.replace(item, "")
-
-    word_as_list = list(word)
-
-    ciphered_word(word_as_list)
+if location > 0:
+    cipher.insert(location, " ")
+cipher = " ".join([str(item) for item in cipher])
+print(f"\n'{original_input}' ciphered using ROT{rotn}: {cipher}")
