022faf2531b932df428722cb1d6aecd92f10aa35
Tyler Giles <tylerjaygiles@gmail.com>
2022-04-05 20:22:35 -0700

    Update Tyler_Giles_Lab-11.py

--- code/tyler/python/Tyler_Giles_Lab-11.py
+++ code/tyler/python/Tyler_Giles_Lab-11.py
@@ -6,22 +6,19 @@
 from string import ascii_lowercase
 alphabet = ascii_lowercase
 alphabet = list(alphabet)
-# print(alphabet)
 
-# user_string = input("\nWhat are you trying to encrypt Mr. Bond? ")
-# user_strength = int(input("How many times would you like to rotate the letters? "))
-user_string = "abc"
-user_string = list(user_string)
-# user_string = list(user_string)
-user_strength = 13
+user_string = input("\nWhat are you trying to encrypt Mr. Bond? ")
+user_strength = int(input("How many times would you like to rotate the letters? "))
+
+
 def encrypt():
-    encrypted_message = []
-    for index in range(len(user_string)):
-        print(user_string.index)
+    encrypted_message = []      # empty list too add message to
+    for letter in user_string:
+         # the letters in the string are passed into the alphabet var and then getting the index associated with it
+         # then adding 
+        encrypted_letter = (alphabet.index(letter) + user_strength) % 26          
+        encrypted_message += alphabet[encrypted_letter]
+    print(str(encrypted_message))
 
-    # for i in range(len(user_string)):
-    #     encrypted_char = user_string[i] + user_strength
-    # encrypted_message.append(encrypted_char)
-    # print(encrypted_message)
 
 encrypt()
