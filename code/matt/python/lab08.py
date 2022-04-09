d253dae3d844f6dcd21b3755b243388de61a63ad
santorsm <matt.santorsola@gmail.com>
2022-04-08 20:40:21 -0700

    1ab08 submission

--- code/matt/python/lab08.py
+++ code/matt/python/lab08.py
@@ -55,25 +55,25 @@ def num_matches(winner: list, ticket: list) -> int:
             # print(f"winner: {winners} = {winnings}")
             balance = balance + winnings
 
-    print(f"{match_count} match(es) in round {COUNTER +1}. Winnings & balance for the round: {winnings}, {balance}")
+    print(f"{match_count} match(es) in round {counter +1}. Winnings & balance for the round: {winnings}, {balance}")
     return balance
 
 
 if __name__ == "__main__":
 
     rounds = input(">>> Enter the number of rounds to play: ")
-    COUNTER = 0
-    ENDING_BALANCE = 0
-    while COUNTER < int(rounds):
+    counter = 0
+    ending_balance = 0
+    while counter < int(rounds):
         house_ticket = pick6(6)
         player_ticket = pick6(6)
-        ENDING_BALANCE += num_matches(house_ticket, player_ticket)
-        COUNTER += 1
-        EXPENSES = COUNTER * -2
-        earnings = EXPENSES - ENDING_BALANCE
-        roi = ((earnings - EXPENSES) / EXPENSES) * 100
-    print(f"\nYour final balance is: {ENDING_BALANCE}\n")
-    print(f"Earnings: {abs(ENDING_BALANCE - EXPENSES)}")
-    print(f"Expenses: {EXPENSES}")
+        ending_balance += num_matches(house_ticket, player_ticket)
+        counter += 1
+        expenses = counter * -2
+        earnings = expenses - ending_balance
+        roi = ((earnings - expenses) / expenses) * 100
+    print(f"\nYour final balance is: {ending_balance}\n")
+    print(f"Earnings: {abs(ending_balance - expenses)}")
+    print(f"Expenses: {expenses}")
     print(f"Return on Investment: {roi}%")
-    print(f"\nYour ending balance is: {ENDING_BALANCE}\n")
+    print(f"\nYour ending balance is: {ending_balance}\n")
