af99f350352c79bfe76c8f9fb773b49d71e0c484
Kris Hernandez <krismac@kriss-air.mynetworksettings.com>
2022-04-11 23:41:15 -0400

    kris_lab_09

--- code/kris/lab_09_bj.py
+++ code/kris/lab_09_bj.py
@@ -3,19 +3,20 @@ card_one = input("\nWhat's your first card(ex. 3 or K)?: ")
 card_two = input("\nWhat's your second card(ex. 3 or K)?: ")
 card_one = card_one.upper()
 card_two = card_two.upper()
-faces = ["J", "Q", "K"]
-ace = "A"
+cards = {
+    "K": 10,
+    "Q": 10,
+    "J": 10,
+    "A": 11
+}
 
-if card_one in faces:
-    card_one = 10
-elif card_one == ace:
-    card_one = 11
+if card_one in cards: 
+    card_one = cards[card_one]
 else: 
     card_one = int(card_one)
-if card_two in faces:
-    card_two = 10
-elif card_two == ace:
-    card_two = 11
+
+if card_two in cards: 
+    card_two = cards[card_two]
 else: 
     card_two = int(card_two)
 added_cards = (card_one + card_two)
