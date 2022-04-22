75f7fff217b1c8b953889c8808ec49deed908c75
Keegan Good <keegood8@gmail.com>
2022-04-21 19:40:20 -0700

    completed adding choices and voting

--- code/keegan/django/django_polls/polls_app/urls.py
+++ code/keegan/django/django_polls/polls_app/urls.py
@@ -8,4 +8,6 @@ urlpatterns = [
     path('', views.index, name="index"),
     path('create-question/', views.create_question, name="create_question"),
     path('add-choices/', views.add_choices, name="add_choices"),
+    # set up a path with a url parameter
+    path('vote/<int:choice_id>', views.vote, name="vote")
 ]
--- code/keegan/django/django_polls/polls_app/views.py
+++ code/keegan/django/django_polls/polls_app/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.urls import reverse
 
 from .models import Question, Choice
 
@@ -25,39 +26,72 @@ def create_question(request):
 
     # grab the values from the form
     question_text = form.get('question-text')
-    choice_count = form.get('choice-count')
+    choice_count = int(form.get('choice-count'))
 
-    # create the new question in the database
-    new_question = Question()
+    # # create the new question in the database
+    # new_question = Question()
 
-    # set the question text
-    new_question.text = question_text
+    # # set the question text
+    # new_question.text = question_text
 
-    # save the object to the database
-    new_question.save()
+    # # save the object to the database
+    # new_question.save()
 
     # shorthand version to create a database object
-    # new_question = Question.objects.create(
-    #     text=question_text
-    # )
-
-
-
-    questions = Question.objects.all()
+    # .create() saves automatically so .save() isn't required
+    new_question = Question.objects.create(
+        text=question_text
+    )
 
     context = {
-        'questions': questions
+        'question': new_question,
+        'choice_count': [number for number in range(1, choice_count + 1)]
     }
-    return render(request, 'polls/index.html', context)
 
+    return render(request, 'polls/add-choices.html', context)
 
 
 
 
 def add_choices(request):
 
-    context = {
-        "choice_count": [0,1,2]
-    }
+    form = request.POST
 
-    return render(request, 'polls/add-choices.html', context)
+    # use the question id to retrieve the 
+    # question with that id from the database
+    
+    # Pull the question_id out of the form data
+    question_id = form.get('question-id')
+    # find the Question object in the database with the question_id
+    question = Question.objects.get(id=question_id)
+
+
+    # loop through all the choices in the form
+    for key in form:
+        if key.startswith('choice'):
+            # get the choice text for the specific choice form field
+            choice_text = form.get(key)
+
+            # create a Choice object in the database
+            # associate the Choice object with the question
+            Choice.objects.create(text=choice_text, question=question)
+
+
+    # redirect to the index url for the polls_app
+    # to reuse the logic from the index() view
+    return redirect(reverse('polls_app:index'))
+
+# choice_id will come from the link that is clicked to vote for the choice
+def vote(request, choice_id):
+    # get the choice from the database using the choice_id from the url parameter
+    choice = Choice.objects.get(id=choice_id)
+
+    # increment the vote count for the clicked vote
+    choice.votes += 1
+
+    # save the changes
+    choice.save()
+
+    # redirect to the index url for the polls_app
+    # to reuse the logic from the index() view
+    return redirect(reverse('polls_app:index'))
