c98f89988badf42c06321f00af9c9cf2e89ab186
violette-git <100663399+violette-git@users.noreply.github.com>
2022-05-14 23:03:48 -0500

    name = pokemon.get(name), AttributeError: str object has no attribute get

---
+++ code/grant/pokedex/pokedex/models.py
@@ -0,0 +1,33 @@
+from django.db import models
+from django.forms import CharField
+
+# Create your models here.
+
+
+
+class PokemonType(models.Model):
+
+    name = models.CharField(max_length=70)
+
+    def __self__(self) -> str:
+
+        return f' {self.name.capitalize} Type Pokemon'
+
+# -----------------------------------------------------------
+
+
+class Pokemon(models.Model):
+
+    name = models.CharField(max_length=70)
+
+    number = models.IntegerField()
+
+    height = models.FloatField()
+
+    weight = models.FloatField()
+
+    image_front = models.CharField(max_length=70)
+
+    image_back = models.CharField(max_length=70)
+
+    types = models.ManyToManyField(PokemonType, related_name='type')
