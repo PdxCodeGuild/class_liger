38fbd644d711f93b6b0c6a4d195a15936fe13f12
Tyler Giles <tylerjaygiles@gmail.com>
2022-05-16 02:51:08 -0700

    update

--- code/tyler/django/django_lab-1/todo_app/views.py
+++ code/tyler/django/django_lab-1/todo_app/views.py
@@ -1,5 +1,28 @@
 from django.shortcuts import render
-
+from .models import TodoItem, models
+from django.urls import reverse
+from django.shortcuts import redirect
 # Create your views here.
+
+
 def index(request):
-    return render(request, 'index.html')
+    todos = TodoItem.objects.all()
+    print(todos)
+    context = {
+        'todos': todos
+    }
+    
+
+    return render(request, 'index.html', context)
+
+def save_todo_item(request):
+    form = request.POST
+    print(form)
+
+    list_item = form.get('todo-text')
+    todos = TodoItem.objects.create(text = todo_text)
+    context = {
+        'todos' : todos,
+    }
+    return redirect(reverse('todo_app:index'))
+
