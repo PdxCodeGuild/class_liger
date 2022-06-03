c4ccd31e4d561fbb1cb0b60cee6f4ac80a7448ea
Clement2022 <clement.buadu@gmail.com>
2022-06-03 01:27:47 -0400

    django_lab03_final_edited

--- code/clement/django/lab03/django_pokedex/pokedex_app/views.py
+++ code/clement/django/lab03/django_pokedex/pokedex_app/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render
 from .models import PokemonType, Pokemon
-from django.db.models import Q
 
 
 # Create your views here.
@@ -13,22 +12,12 @@ def index(request):
     pokemon_search = request.POST.get('pokemon-search') or ''
 
     if pokemon_search:
-        # Using Q object to search within the name & types fields for pokemon_search
-        pokemons = Pokemon.objects.filter(
-            Q(name__icontains=pokemon_search) | Q(
-                types__name__icontains=pokemon_search)
-        )
-
-    form_data = {
-        'pokemon_search': pokemon_search,
-
-    }
+        # Using object to search within the name & types fields for pokemon_search
+        pokemons = Pokemon.objects.filter(name__icontains=pokemon_search)
 
     context = {
         'pokemon_search': pokemon_search,
         'pokemons': pokemons,
-        'form_data': form_data,
-
     }
 
     return render(request, 'pokedex/index.html', context)
